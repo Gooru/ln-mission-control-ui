4d4ea3acfb93063024b596925fde00b489394aa7
arunkumar <arunbalan005@gmail.com>
2019-12-17 23:21:53 +0530

    changed card name

--- src/routers/views/network/country-drill-down/texas-cards-list/texas-cards-list.vue
+++ src/routers/views/network/country-drill-down/texas-cards-list/texas-cards-list.vue
@@ -16,7 +16,7 @@
       <top-providers />
     </div>
     <div class="class-activities-card">
-      <new-user-card iconName="people" :cardValue="cardDetails.gained_comp" cardTitle="Competencies Gained" cardName="class-activity"/>
+      <new-user-card  :cardValue="cardDetails.gained_comp" cardTitle="of students gained > 10 competencies" cardName="class-activity"/>
     </div>
      <div class="struggling-competency">
       <struggling-competency :strugglingCompetencies="cardDetails.stugglingCompetency" />
