cf476d5dfc799e95b8f5d22bc1a90af20fe2881e
Gugan <gugankumar.p@bambeeq.com>
2020-01-07 18:28:02 +0530

    Updated date month picker to allow carousel in time series

--- src/components/selector/month-year-picker/month-year-picker.ts
+++ src/components/selector/month-year-picker/month-year-picker.ts
@@ -1,23 +1,33 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import {getDateRangeArray} from '@/utils/date';
+import {getDateRangeArray, subtractMonth, addMonth} from '@/utils/date';
 import { getFormattedYear, getFormattedMonth } from '@/helpers/date-format';
+import GoogleMaterialIcon from '@/components/icons/google-material-icon/google-material-icon';
 import moment from 'moment';
 
 @Component({
   name: 'month-year-picker',
+  components: {
+    'google-material-icon': GoogleMaterialIcon,
+  },
 })
 export default class MonthYearPicker extends Vue {
 
-  private startDate = moment();
+  private startDate: string = moment().subtract(11, 'month').format('YYYY-MM-01');
 
-  private endDate = moment();
+  private endDate: string = moment().format('YYYY-MM-01');
 
   private dateRangeList: any = [];
 
   private activeDate: string = moment().format('YYYY-MM-01');
 
-  @Prop()
+  private currentMonth: string = moment().format('YYYY-MM-01');
+
+  private carouselMonths: number = 2;
+
   private activeYear!: string;
+
+  @Prop()
+  private startYear!: string;
 
   private created() {
     this.loadDateRange();
@@ -28,9 +38,32 @@ export default class MonthYearPicker extends Vue {
     this.$emit('onChageTimeline', date);
   }
 
+  private onSelectYear() {
+    const year = 2017;
+    this.startDate = moment().format(`${year}-MM-01`);
+    this.endDate = addMonth(this.startDate, 11);
+    this.loadDateRange();
+  }
+
+  private onPopulateMonth(direction: string) {
+    const component = this;
+    let startDate = component.startDate;
+    let endDate = component.endDate;
+    if (direction === 'left') {
+      startDate = subtractMonth(startDate, this.carouselMonths);
+      endDate = subtractMonth(endDate, this.carouselMonths);
+    } else {
+      startDate = addMonth(startDate, this.carouselMonths);
+      endDate = addMonth(endDate, this.carouselMonths);
+    }
+    component.startDate = startDate;
+    component.endDate = endDate;
+    this.loadDateRange();
+  }
+
   private loadDateRange() {
-    const startDate = '01-01-2019';
-    const endDate = '12-01-2019';
+    const startDate = this.startDate;
+    const endDate = this.endDate;
     const dateRangeList = getDateRangeArray(startDate, endDate, 'month');
     this.dateRangeList = dateRangeList;
   }
