03e5c91277acaec99b8ea543cab704e90b640d4c
arunkumar <arunbalan005@gmail.com>
2020-01-23 23:23:24 +0530

    added avatar profile

--- src/routers/views/competency-map/competency-map.ts
+++ src/routers/views/competency-map/competency-map.ts
@@ -1,5 +1,10 @@
-import {Vue, Component} from 'vue-property-decorator';
+import { Vue, Component, Watch } from 'vue-property-decorator';
 import GoogleMaterialIcon from '@/components/icons/google-material-icon/google-material-icon';
+import axios from 'axios';
+import { SubjectModel } from '@/models/taxonomy/subject';
+import { ClassificationModel } from '@/models/taxonomy/classification';
+import { taxonomyAPI } from '@/providers/apis/taxonomy/taxonomy';
+import { sessionService } from '@/providers/services/auth/session';
 
 @Component({
     name: 'competency-map',
@@ -9,5 +14,59 @@ import GoogleMaterialIcon from '@/components/icons/google-material-icon/google-m
 })
 
 export default class CompetencyMap extends Vue {
+
+    private subjectList?: any = [];
+
+    private selectedCategory: any = {};
+
+    private selectedSubject: any = {};
+
+    private defaultCategoryId: string = 'k_12';
+
+    private defaultSubjectCode: string = 'K12.MA';
+
+    private categories?: ClassificationModel[] = [];
+
+    @Watch('subjectList')
+    private createAvatar(subjects: any) {
+        if (subjects.length) {
+            subjects.map((subject: any) => {
+                subject.color = '#' + (Math.random().toString(16).slice(-6));
+            });
+        }
+    }
+
+    private userId() {
+        const session = sessionService.getSession();
+        return session ? session.user_id : '';
+    }
+
+    private onSelectCategory(category: any) {
+        this.selectedCategory = category;
+        this.subjectsByCategory(category.id)
+            .then((subjects) => {
+                this.subjectList = subjects;
+            });
+    }
+
+    private created() {
+        this.fetchTaxonomyData();
+    }
+
+    private fetchTaxonomyData() {
+        axios.all([
+            taxonomyAPI.fetchTaxonomyClassifications(),
+            this.subjectsByCategory(this.defaultCategoryId),
+
+        ]).then(axios.spread((classifications, subjects) => {
+            this.categories = classifications;
+            this.subjectList = subjects;
+            this.selectedCategory = this.categories.find((category) => category.id === this.defaultCategoryId);
+        }));
+    }
+
+    private subjectsByCategory(categoryId: string) {
+        return taxonomyAPI.fetchTaxonomySubjects(categoryId);
+    }
 
 }
--- src/routers/views/competency-map/competency-map.vue
+++ src/routers/views/competency-map/competency-map.vue
@@ -13,32 +13,46 @@
             <div class="mc-competency-content-section">
                 <div class="competency-left-section">
                     <div class="category-header">
-                         <b-dropdown class="categories-dropdown" text="Selection of Categories" variant="none">
-                            <b-dropdown-item href="#">Selection of Categories</b-dropdown-item>
-                            <b-dropdown-item href="#">Another action</b-dropdown-item>
-                            <b-dropdown-item href="#">Something else here</b-dropdown-item>
+                         <b-dropdown class="categories-dropdown" :text="selectedCategory ? selectedCategory.title : ''" variant="none">
+                            <b-dropdown-item href="#" 
+                            v-for="(category, index) in categories"  
+                            :key="index" 
+                            v-on:click="onSelectCategory(category)">{{category.title}}
+                            </b-dropdown-item>
                         </b-dropdown>
                        <span class="header-subtitle">COMMON CORE STATE STANDARDS</span>
                     </div>
                     <div class="subject-list-container">
                         <h5 class="subject-header">SUBJECT</h5>
                         <div class="subject-list-section">
-                            <div class="subject-detail">
-                                <div class="subject-thumbnail"></div>
-                                <div class="subject-title">Biology</div>
+                            <div class="subject-detail" v-for="(subject, index) in subjectList" :key="index">
+                                <div class="subject-thumbnail" :style="{backgroundColor: subject.color}">{{subject.title | avatarText}}</div>
+                                <div class="subject-title">{{subject.title}}</div>
                             </div> 
                         </div>
                     </div>
                     <div class="create-course">
-                        <b-button variant="secondary" block disabled>Create Course</b-button>
+                        <b-button variant="light" block>Create Course</b-button>
                     </div>
                 </div>
-                <div class="competency-right-section"></div>
+                <div class="competency-right-section">
+                    <div class="competency-cards-list">
+                        <div class="competency-card-panel" v-for="(subject, index) in subjectList" :key="index">
+                            <div class="competenct-card-header">
+                                <div class="card-thumbnail" :style="{backgroundColor: subject.color}">{{subject.title | avatarText}}</div>
+                                <div class="subject-name">{{subject.title}}</div>
+                            </div>
+                            <div class="competency-card-body">
+                                <div class="matrix-chart"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" src="./competency-map.ts"></script>
-<style lang="scss" scoped src="./competency-map.scss"></style>
+<style lang="scss" src="./competency-map.scss"></style>
 
