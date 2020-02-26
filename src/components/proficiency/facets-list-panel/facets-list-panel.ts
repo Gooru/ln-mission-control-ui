f12cefa45ed62c06a2d0140915e42c05b21f5dc0
Gugan <gugankumar.p@bambeeq.com>
2020-02-26 18:30:18 +0530

    Integrated facet selection

--- src/components/proficiency/facets-list-panel/facets-list-panel.ts
+++ src/components/proficiency/facets-list-panel/facets-list-panel.ts
@@ -1,10 +1,12 @@
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 import GoogleMaterialIcon from '@/components/icons/google-material-icon/google-material-icon';
+import PortfolioStatCard from '@/components/cards/portfolio-stat-card/portfolio-stat-card';
 
 @Component({
   name: 'facets-list-panel',
   components: {
     'google-material-icon': GoogleMaterialIcon,
+    'portfolio-stat-card': PortfolioStatCard,
   },
 })
 export default class FacetsListPanel extends Vue {
@@ -13,5 +15,9 @@ export default class FacetsListPanel extends Vue {
   private facetsCompetencyMatrix!: any;
 
   private isShowCompetencyDetails: boolean = false;
+
+  private onSelectPortfolioStat(portfolioStat: any) {
+    this.$emit('onSelectPortfolioStat', portfolioStat);
+  }
 
 }
--- src/components/proficiency/facets-list-panel/facets-list-panel.vue
+++ src/components/proficiency/facets-list-panel/facets-list-panel.vue
@@ -5,30 +5,12 @@
       v-for="(facetCompetencyMatrix, index) in facetsCompetencyMatrix"
       v-bind:key="index"
     >
-      <div class="facet-header">
-        <div class="facet-seq">{{index + 1}}.</div>
-        <div class="facet-info">
-          <span class="facet-title">{{facetCompetencyMatrix.subjectName}}</span>
-          <span
-            class="total-competencies"
-          >{{facetCompetencyMatrix.totalCompetenciesCount}} Competencies</span>
-        </div>
-        <div class="toggle" @click="isShowCompetencyDetails = !isShowCompetencyDetails">
-          <google-material-icon :icon="isShowCompetencyDetails ? 'arrow_drop_up' : 'arrow_drop_down'" />
-        </div>
-      </div>
-      <div class="facet-body" v-if="isShowCompetencyDetails">
-        <div
-          class="facet-status"
-          v-for="(competencyCount, competencySeq) in facetCompetencyMatrix.competenciesCount"
-          v-bind:key="competencySeq"
-          v-b-tooltip.hover
-          :title="competencyCount.status"
-        >
-          <span class="box" v-bind:class="competencyCount.status"></span>
-          <span class="count">{{competencyCount.count}}</span>
-        </div>
-      </div>
+      <portfolio-stat-card
+        :statInfo="facetCompetencyMatrix"
+        statType="facetCompetencies"
+        :sequence="index + 1"
+        @onSelectPortfolioStat="onSelectPortfolioStat"
+      />
     </div>
   </div>
 </template>
